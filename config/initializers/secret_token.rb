5683dd8ef9d6253c92ed906459846b9b5d500b56
David Benhaim <davidbenhaim12@gmail.com>
2012-09-09 17:16:13 -0400

    getting started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbenhaimProj1::Application.config.secret_token = 'fbdf54538fa5232ac4608019e873e844ae4026a7179d38c846256e2d5186773c51bfd4a1aea2ad777b45663491c6088a47d5b658475bfe5f509dd8cefb06a3d7'

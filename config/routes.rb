53a4b1a5c3097364657957c97d3864eaa775457b
David Benhaim <davidbenhaim12@gmail.com>
2012-09-09 21:53:30 -0400

    learning ruby/rails and getting more and more used to it. reminds me of coffeescript :). added site object/scaffolding and visited function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 DbenhaimProj1::Application.routes.draw do
+  resources :sites
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match 'sites/:name/visit' => 'sites#visited'
+  # match 'sites/' => 'sites#index'
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
